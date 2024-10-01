ab5a13090b2ab063b271ee64f9938a704260cf58
Russellouo <rchien25_student@ivycollegiate.org>
2024-10-01 06:21:06 +0000

    1.75

---
+++ Russell_Sprint_Projectv1.7/Russell_Sprint_Projectv1.75.py
@@ -0,0 +1,13 @@
+import math
+
+def calculate_root(a, b, c):
+    discriminate = b**2 - 4*a*c
+
+    if discriminate > 0:
+    
+    elif discriminate == 0:
+
+    else:
+        return None
+print(calculate_root(1, -5, 6))
+print(calculate_root(1, 2, 1))
