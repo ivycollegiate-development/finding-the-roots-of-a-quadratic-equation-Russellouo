dc0820b57f8b4bb1741624d04c4737d2c07c3669
Russellouo <rchien25_student@ivycollegiate.org>
2024-10-04 05:51:23 +0000

    10/4

--- Russell_Sprint_Projectv1.7/Russell_Sprint_Projectv1.75.py
+++ Russell_Sprint_Projectv1.7/Russell_Sprint_Projectv1.75.py
@@ -3,11 +3,11 @@ import math
 def calculate_root(a, b, c):
     discriminate = b**2 - 4*a*c
 
-    if discriminate > 0:
-    
-    elif discriminate == 0:
 
-    else:
-        return None
-print(calculate_root(1, -5, 6))
-print(calculate_root(1, 2, 1))
+
+    root_1 = 
+
+    root_2 =
+    return root_1, root_2
+
+print(calculate_root(1, 1, 1))
